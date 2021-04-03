7936074f518a5f113a487efe66d408d835ca1388
FayadAl-Habsyi <fayedalhabsyi636@gmail.com>
2021-04-03 12:14:31 +0800

    Membuat Program Input Output

---
+++ biodata.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class biodata{
+   public static void main(String [] args){
+
+System.out.println("****** Biodata ******");
+    
+   Scanner scanner = new Scanner(System.in);
+
+	
+	String fayad;
+	int Nim;
+
+ System.out.print("Masukkan Nama Anda: ");
+ fayad= scanner.nextLine();
+ System.out.print("Masukkan Nim Anda: ");
+ Nim= scanner.nextInt();
+ System.out.println("Nama Anda: "+fayad);
+ System.out.println("Nama Anda: "+Nim);
+
+}
+}
+ 
